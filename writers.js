ab3136f15020d135b0a0761b2adb1bff46ace446
shayari-library <yashvigupta238@gmail.com>
2026-09-01 23:58:45 +0530

    Add files via upload

---
+++ writers.js
@@ -0,0 +1,17 @@
+const writers = {
+
+    "Jaun Elia": {
+        folder: "images/jaun",
+        images: [
+            "jaun1.jpg.png"
+        ]
+    },
+
+    "Mirza Ghalib": {
+        folder: "images/ghalib",
+        images: [
+            "ghalib1.jpg.png"
+        ]
+    }
+
+};
